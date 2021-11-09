f4cad6738264ffc1bd4eaa1690a51bed61aac3e5
sachin <sachin@sachins-MacBook-Pro.local>
2021-11-09 19:30:30 +0530

    add hover switcher

--- assets-api/custom.js
+++ assets-api/custom.js
@@ -1,4 +1,33 @@
+switchSections = [
+    {"title": "Who we are?", "description": "We are Guni, a software company based in paramatta, Australia, who strives to provide businesses with tools that would help in better engagement and growth.", "image": "assets-api/img/about/about-1.png"},
+    {"title": "What do we do?", "description": "We aim to improve upon the means of communication for businesses all across the world by leveraging technology.", "image": "https://images.unsplash.com/photo-1550795598-717619d32900?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"},
+    {"title": "How do we do it?", "description": "Our messaging platform helps businesses in engaging with their key stakeholders through an omnichannel approach. With engagement tools such as SMS, MMS, Landing Pages, Email and Vice-Versa and Chatbots, your business could deliver the right message, on the right set of channels, at the right moment.", "image": "https://images.unsplash.com/photo-1550133804-0d34e3130759?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"},
+];
+let aboutActiveTitleEle = $("p#about-active-title");
+let aboutActiveImgEle = $("img#about-active-img");
+let aboutActiveDescEle = $("p#about-active-desc");
+let defaultAbout = switchSections[0];
+
+const switchAboutContent = function(thisEle, replaceObj, defaultObj) {
+    aboutActiveTitleEle.html(replaceObj.title);
+    aboutActiveImgEle.attr("src", replaceObj.image);
+    aboutActiveDescEle.html(replaceObj.description);
+
+    thisEle.find("div.feature-about-head > p").html(defaultObj.title)
+    thisEle.find("div.feature-about-body > p").html(defaultObj.description);
+}
+
 $(function() {
+    $(".about-default").hover(function() {
+        let switchSectionIndex = $(this).data('index') || 0;
+        let activeAbout = switchSections[switchSectionIndex];
+        switchAboutContent($(this), activeAbout, defaultAbout);
+    }, function() {
+        let switchSectionIndex = $(this).data('index') || 0;
+        let activeAbout = switchSections[switchSectionIndex];
+        switchAboutContent($(this), defaultAbout, activeAbout);
+    });
+
     $('.slider-thumb').slick({
         autoplay: false,
         vertical: true,
